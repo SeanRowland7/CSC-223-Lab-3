b048630ab408ca6d8e8cb86c1e8822a45361358f
WorseHabits <117957010+WorseHabits@users.noreply.github.com>
2023-02-13 11:09:06 -0500

    added some more EquivalenceClasses tests

--- src/utilities/Comparators.java
+++ src/utilities/Comparators.java
@@ -4,13 +4,33 @@ import java.util.Comparator;
 
 public class Comparators {
 
-	static Comparator<Integer> simple = new Comparator<Integer>() {
+	static Comparator<Integer> intCompare = new Comparator<Integer>() {
 		
 		public int compare(Integer x, Integer y) {
 			
 			return x % 2 == y % 2 ? 0 : 1;
 			
 		}
+	};
+	
+	static Comparator<String> lengthCompare = new Comparator<String>() {
+		
+		public int compare(String x, String y) {
+			
+			return x.length() % 2 == y.length() % 2 ? 0 : 1;
+			
+		}
+		
+	};
+	
+	static Comparator<Integer> negativeCompare = new Comparator<Integer>() {
+		
+		public int compare(Integer x, Integer y) {
+			
+			return (x < 0) == (y < 0) ? 0 : 1;
+			
+		}
+		
 	};
 	
 }
--- test/utilities/EquivalenceClassesTest.java
+++ test/utilities/EquivalenceClassesTest.java
@@ -10,7 +10,7 @@ class EquivalenceClassesTest {
 	void testBuild() 
 	{
 		
-		EquivalenceClasses<Integer> test = new EquivalenceClasses<Integer>(Comparators.simple);
+		EquivalenceClasses<Integer> test = new EquivalenceClasses<Integer>(Comparators.intCompare);
 		
 		assertEquals(0, test.size());
 		
@@ -19,9 +19,9 @@ class EquivalenceClassesTest {
 	}
 	
 	@Test
-	void testAdd() {
+	void testModAdd() {
 		
-		EquivalenceClasses<Integer> test = new EquivalenceClasses<Integer>(Comparators.simple);
+		EquivalenceClasses<Integer> test = new EquivalenceClasses<Integer>(Comparators.intCompare);
 		
 		test.add(1);
 		
@@ -30,6 +30,47 @@ class EquivalenceClassesTest {
 		assertEquals(1, test.numClasses());
 		
 		assertTrue(test.contains(1));
+		
+		assertEquals(0, test.indexOfClass(1));
+		
+	}
+	
+	@Test
+	void testNegativeAdd() {
+		
+		EquivalenceClasses<Integer> test = new EquivalenceClasses<Integer>(Comparators.negativeCompare);
+		
+		test.add(-1);
+		
+		test.add(1);
+		
+		assertEquals(2, test.size());
+		
+		assertEquals(2, test.numClasses());
+		
+		test.add(2);
+		
+		assertEquals(3, test.size());
+		
+		assertEquals(2, test.numClasses());
+		
+		
+	}
+	
+	@Test
+	void testNumClasses() {
+		
+		EquivalenceClasses<Integer> test = new EquivalenceClasses<Integer>(Comparators.intCompare);
+		
+		for(int i = 1; i < 101; i++) {
+			
+			test.add(i);
+			
+		}
+		
+		assertEquals(2, test.numClasses());
+		
+		assertEquals(100, test.size());
 		
 	}
 
